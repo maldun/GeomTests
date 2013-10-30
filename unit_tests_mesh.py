ca9c8d4594a5f341335306d35941a07a0cb4cee2
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2013-10-30 15:44:55 +0100

    added tests

--- unit_tests_mesh.py
+++ unit_tests_mesh.py
@@ -32,11 +32,13 @@ class UnitTester(object):
         ids_tri = mesh.GetIdsFromFilter(filter_tri)
         tria3 = Tria3(mesh,ids_tri[0])
 
+        tria3.computeArea()
+        tria3.computeNormal()
+
         print('Tria3 Tests: ',
+              tria3.getNodes(),
               tria3.computeNormalOp(),
-              tria3.computeArea(),
               tria3.getArea(),
-              tria3.computeNormal(),
               tria3.getNormal(),
               )
 
@@ -46,11 +48,13 @@ class UnitTester(object):
         ids_quad = mesh.GetIdsFromFilter(filter_quad)
         quad4 = Quad4(mesh,ids_quad[0])
 
+        quad4.computeArea()
+        quad4.computeNormal()
+
         print('Quad4 Tests: ',
+              quad4.getNodes(),
               quad4.computeNormalOp(),
-              quad4.computeArea(),
               quad4.getArea(),
-              quad4.computeNormal(),
               quad4.getNormal(),
               )
 
