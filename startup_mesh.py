73dda2e62c16ccb2d88332b878d5a9323c49dc06
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2013-11-12 08:31:24 +0100

    added missing startup file for mesh

---
+++ startup_mesh.py
@@ -0,0 +1,8 @@
+import os
+home = os.path.expanduser("~/")
+
+os.chdir(home + "prog/Python/salome")
+
+from salome import ImportComponentGUI
+gg = ImportComponentGUI("GEOM")
+from GeomTests import unit_tests_mesh
