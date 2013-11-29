8d08f4d533e47eef1c4a7c8ee3027aa662f8c7bb
Stefan Reiterer <maldun.finsterschreck@gmail.com>
2013-11-29 21:41:43 +0100

    finished first test series for mean curvature formula

--- unit_tests_mesh.py
+++ unit_tests_mesh.py
@@ -53,6 +53,7 @@ class UnitTester(object):
         self.testQuad4()
         self.testNormalVectorField()
         self.testNormalVectorFieldProbs()
+        self.testMeanCurvatureStuff()
 
     def testTria3(self):
         mesh = find_object('Mesh_1')
@@ -64,12 +65,18 @@ class UnitTester(object):
         tria3.computeArea()
         tria3.computeNormal(tria_node1)
 
+        # Test the formula for mean normal curvature
+        # center node id = 2; element to test 19
+        mesh4 = find_mesh('Mesh_4')
+        tria3C = Tria3(mesh4,19)
+        
         print('Tria3 Tests: ',
               tria3.getNodes(),
               tria3._computeNormalOp(),
               tria3.getArea(),
               tria3.getNormal(tria_node1),
-              tria3.getNormals()
+              tria3.getNormals(),
+              norm(tria3C.getCurvatureVector(2)-array([-0.5,0.5,0.0])) < 0.01,
               )
 
     def testQuad4(self):
@@ -82,12 +89,18 @@ class UnitTester(object):
         quad4.computeArea()
         quad4.computeNormal(quad4_node1)
 
+        #Test mean curvature vector
+        # Center id = 9; element id = 12
+        mesh_cq = find_mesh('Mesh_curv_quad')
+        quad4C = Quad4(mesh_cq,12)
+
         print('Quad4 Tests: ',
               quad4.getNodes(),
               quad4._computeNormalOp(),
               quad4.getArea(),
               #quad4.getNormal(quad4_node1),
-              quad4.getNormals()
+              quad4.getNormals(),
+              norm(quad4C.getCurvatureVector(9)-array([0.0,1.0,0.0])) < 0.01,
               )
 
     def testNormalVectorField(self):
@@ -158,6 +171,28 @@ class UnitTester(object):
         except NotImplementedError:
             print('Test normalvector problematic cases: correct error handling')
 
+    def testMeanCurvatureStuff(self):
+        mesh4 = find_mesh('Mesh_4')
+        norm_field4 = NormalVectorField(mesh4)
+        # get triangles
+        filter_tri = smesh.GetFilter(smesh.FACE, smesh.FT_ElemGeomType, smesh.Geom_TRIANGLE)
+        ids_tri = mesh4.GetIdsFromFilter(filter_tri)
+        tria_elems = [Tria3(mesh4,id_tri) for id_tri in ids_tri]
+        print('test mean curvature Formula for tria3: ',
+              norm(norm_field4.meanNormalCurvatureFormula(tria_elems,2)) < 1e-10,
+            ) 
+        
+        # quadrangles
+        mesh_cq = find_mesh('Mesh_curv_quad')
+        norm_field_cq = NormalVectorField(mesh_cq)
+        
+        filter_q = smesh.GetFilter(smesh.FACE, smesh.FT_ElemGeomType, smesh.Geom_QUADRANGLE)
+        ids_q = mesh_cq.GetIdsFromFilter(filter_q)
+        quad_elems = [Quad4(mesh_cq,id_q) for id_q in ids_q]
+        print('test mean curvature Formula for quad4: ',
+              norm(norm_field_cq.meanNormalCurvatureFormula(quad_elems,9)) < 1e-10,
+            ) 
+        
     def __init__(self):
 
         self.testTypes()
